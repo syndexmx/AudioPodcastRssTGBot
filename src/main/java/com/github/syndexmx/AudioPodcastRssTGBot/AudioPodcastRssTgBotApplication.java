204ae08888e5fcd941aa9ddcc20ca3dce66bea5c
Maxim Shapovalov <syndex@list.ru>
2024-12-24 14:45:05 +0300

    Initial commit

---
+++ src/main/java/com/github/syndexmx/AudioPodcastRssTGBot/AudioPodcastRssTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.AudioPodcastRssTGBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioPodcastRssTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioPodcastRssTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/syndexmx/AudioPodcastRssTGBot/AudioPodcastRssTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.AudioPodcastRssTGBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioPodcastRssTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
